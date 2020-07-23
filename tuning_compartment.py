6760a0d936b55526a79b6264ece225cd9b1d43ec
Geoff deRosenroll <geoffderosenroll@gmail.com>
2020-07-23 16:14:27 -0700

    WIP implementing onset time assignment.

--- tuning_compartment.py
+++ tuning_compartment.py
@@ -40,11 +40,20 @@ class TuningToy:
             "E": {"pref": 0.95, "null": 0.05},
             "I": {"pref": 0.95, "null": 0.05},
         }
+        self.syn_timing = {
+            "E": {"var": 15, "delay": 5},
+            "I": {"var": 15, "delay": 0},
+        }
+
         self.dir_labels = [225, 270, 315, 0, 45, 90, 135, 180]
         self.dir_rads   = np.radians(self.dir_labels)
         self.dirs       = [135, 90, 45, 0, 45, 90, 135, 180]
         self.dir_inds   = np.array(self.dir_labels).argsort()
         self.circle     = np.deg2rad([0, 45, 90, 135, 180, 225, 270, 315, 0])
+
+        self.light_bar = {
+            "speed": 1., "x_motion": True, "x_start": -60, "y_start": -70
+        }
 
         self.config_soma()
         self.create_synapse()
@@ -174,19 +183,79 @@ class TuningToy:
             s: {
                 "X": self.origin["X"] - self.offset * np.cos(np.deg2rad(t)),
                 "Y": self.origin["Y"] - self.offset * np.sin(np.deg2rad(t)),
-            } for s, t in thetas.items() 
+            } for s, t in thetas.items()
         }
 
         self.prs = {
             s: [
                 pn["pref"] + (pn["null"] - pn["pref"]) * (
                     1 - 0.98 / (1 + np.exp((wrap_180(np.abs(t - d)) - 91) / 25))
-                ) for d in self.dir_labels 
+                ) for d in self.dir_labels
             ] for (s, t), pn in zip(thetas.items(), self.dir_pr.values())
         }
 
         # theta difference used to scale down rho
         self.delta = wrap_180(np.abs(thetas["E"] - thetas["I"]))
+
+    def rotate_sacs(self, rotation):
+        rotated = {}
+        for s, locs in self.bp_locs.items():
+            x, y = rotate(self.origin, locs["X"], locs["Y"], rotation)
+            rotated[s] = {"X": x, "Y": y}
+        return rotated
+
+    def bar_sweep(self, dir_idx):
+        """Return activation time for the single synapse based on the light bar
+        config and the bipolar locations on the presynaptic dendrites.
+        """
+        bar = self.light_bar
+        ax = "x" if bar["x_motion"] else "y"
+        locs = rotate_sacs(-self.dir_rads[dir_idx])
+
+        on_times = {
+            s: bar["start_time"] + (loc[ax] - bar[ax + "_start"] / bar["speed"])
+            for s, loc in locs.items()
+        }
+
+        return on_times
+
+    def bar_onsets(self, stim, locked_synapses=None):
+        """
+        Calculate onset times for each synapse based on when the simulated bar
+        would be passing over their location, modified by spatial offsets.
+        Timing jitter is applied using pseudo-random number generators.
+        """
+        sac = self.sac_net
+
+        time_rho = stim.get("rhos", {"time": self.time_rho})["time"]
+
+        if (self.sac_angle_rho_mode):
+            syn_rho = time_rho
+        else:
+            syn_rho = time_rho - time_rho * self.deltas / 180
+
+        # bare base onset with added shared jitter
+        jit = h.Random(self.seed).jit_rand.normal(0, 1)
+        self.seed += 1
+        bar_times = {
+            k: v + jit * self.jitter
+            for k, v in self.bar_sweep(stim["dir"]).items()
+        }
+
+        rand_on = {}
+        for t in self.bar_times.keys():
+            rand_on[t] = h.Random(self.seed).normal(0, 1)
+            self.seed += 1
+
+        rand_on["E"] = rand_on["I"] * syn_rho + (
+            rand_on["E"] * np.sqrt(1 - syn_rho ** 2)
+        )
+
+        for t, timing in self.syn_timing.items():
+            self.syns[t]["stim"].start = (
+                bar_times[t] + timing["delay"] + rand_on[t] * timing["var"]
+            )
+
 
 def set_hoc_params():
     """Set hoc NEURON environment model run parameters."""
